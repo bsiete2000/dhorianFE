1bd3d81d45e4e7ce76afc97897959703ab6c9352
bsiete2000 <jrojas@quefacilweb.com>
2015-12-09 17:16:44 -0500

    preview contacts
    This reverts commit ca6bb21dbfc7e567e1786d8cd441d24f691e6677.
    
    # Conflicts:
    #	pages/tables/contacts.html

--- dist/js/app.js
+++ dist/js/app.js
@@ -977,7 +977,7 @@ $(document).ready(function() {
 
 /*autocomplete tags para los filtros*/
         
-var myApp2 = angular.module('myApp2', [])
+var myApp = angular.module('myApp2', [])
 .directive('tagManager', function() {
   
     return {
@@ -992,7 +992,7 @@ var myApp2 = angular.module('myApp2', [])
           ' <div ng-repeat="(idx, tag) in tags" class="tag label label-success">{{tag}} <a class="close" href ng-click="remove(idx)">×</a></div>' +
           ' <input type="text" placeholder="agregar..." ng-model="newValue" />' +
           '</div>' +
-            ' <a class="btn btn-default" ng-click="add()">Agreagar filtro</a>' +
+            ' <a class="btn btn-primary" href="#" data-toggle="modal" data-target="#myModal3">Agreagar filtro</a>' +
           '</div>',
         link: function ( $scope, $element ) {
             
@@ -1084,3 +1084,83 @@ var myApp2 = angular.module('myApp2', [])
 
 
 //$(document).ready(function() {});
+
+//checkboxes filtros//
+$(function () {
+    $('.list-group.checked-list-box .list-group-item').each(function () {
+        
+        // Settings
+        var $widget = $(this),
+            $checkbox = $('<input type="checkbox" class="hidden" />'),
+            color = ($widget.data('color') ? $widget.data('color') : "primary"),
+            style = ($widget.data('style') == "button" ? "btn-" : "list-group-item-"),
+            settings = {
+                on: {
+                    icon: 'glyphicon glyphicon-check'
+                },
+                off: {
+                    icon: 'glyphicon glyphicon-unchecked'
+                }
+            };
+            
+        $widget.css('cursor', 'pointer')
+        $widget.append($checkbox);
+
+        // Event Handlers
+        $widget.on('click', function () {
+            $checkbox.prop('checked', !$checkbox.is(':checked'));
+            $checkbox.triggerHandler('change');
+            updateDisplay();
+        });
+        $checkbox.on('change', function () {
+            updateDisplay();
+        });
+          
+
+        // Actions
+        function updateDisplay() {
+            var isChecked = $checkbox.is(':checked');
+
+            // Set the button's state
+            $widget.data('state', (isChecked) ? "on" : "off");
+
+            // Set the button's icon
+            $widget.find('.state-icon')
+                .removeClass()
+                .addClass('state-icon ' + settings[$widget.data('state')].icon);
+
+            // Update the button's color
+            if (isChecked) {
+                $widget.addClass(style + color + ' active');
+            } else {
+                $widget.removeClass(style + color + ' active');
+            }
+        }
+
+        // Initialization
+        function init() {
+            
+            if ($widget.data('checked') == true) {
+                $checkbox.prop('checked', !$checkbox.is(':checked'));
+            }
+            
+            updateDisplay();
+
+            // Inject the icon if applicable
+            if ($widget.find('.state-icon').length == 0) {
+                $widget.prepend('<span class="state-icon ' + settings[$widget.data('state')].icon + '"></span>');
+            }
+        }
+        init();
+    });
+    
+    $('#get-checked-data').on('click', function(event) {
+        event.preventDefault(); 
+        var checkedItems = {}, counter = 0;
+        $("#check-list-box li.active").each(function(idx, li) {
+            checkedItems[counter] = $(li).text();
+            counter++;
+        });
+        $('#display-json').html(JSON.stringify(checkedItems, null, '\t'));
+    });
+});
